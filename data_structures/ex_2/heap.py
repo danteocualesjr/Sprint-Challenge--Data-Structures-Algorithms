acc0a48f56e5646c49a31c272af81026691807b4
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-07-21 01:23:07 +0800

    Implemented heapsort method in heap.py.

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,5 +1,53 @@
 def heapsort(arr):
-  pass
+  heap = Heap()
+  sorted_list = []
+  for item in arr:
+    heap.insert(item)
+  while heap.size > 0:
+    sorted_list.insert(0, heap.delete())
+  return sorted_list
+
+# def swap(arr, i, j):
+#   arr[i], arr[j] = arr[j], arr[i]
+
+# def is_heap(arr):
+#   n = 0
+#   m = 0
+#   while True:
+#     for i in [0, 1]:
+#       m += 1
+#       if m > len(a):
+#         return True
+#       if a[m] > a[n]:
+#         return False
+#     n += 1
+
+# def sift_down(arr, n, max):
+#   while True:
+#     biggest = n
+#     c1 = 2 * n + 1
+#     c2 = c1 + 1
+#     for c in [c1, c2]:
+#       if c < max and arr[c] > a[biggest]:
+#         biggest = c
+#       if biggest == n:
+#         return
+#       swap(a, n, biggest)
+#       n = biggest
+
+# def heapify(arr):
+#   i = len(arr) / 2 - 1
+#   max = len(arr)
+#   while i >= 0:
+#     sift_down(arr, i, max)
+#     i -= 1
+
+# def heapsort(arr):
+#   heapify(arr)
+#   j = len(arr) - 1
+#   while j > 0:
+#     swap(arr, 0, j)
+#     j -= 1
 
 class Heap:
   def __init__(self):
